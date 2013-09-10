094b60db2042ce8eea8254afd5589d3c212f8f6f
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-09-10 15:27:07 +0900

    use insert_into_file instead of gsub_file

--- remotipart.rb
+++ remotipart.rb
@@ -5,8 +5,4 @@
 #  rake rails:template LOCATION=path/to/remotipart.rb
 
 gem 'remotipart'
-gsub_file 'app/assets/javascripts/application.js', %r"(?<=//= require jquery_ujs\n)(?!.*remotipart)"m do
-  <<-JS
-//= require jquery.remotipart
-  JS
-end
+insert_into_file 'app/assets/javascripts/application.js', "//= require jquery.remotipart\n", after: "//= require jquery_ujs\n"
