911fa0ce47ae9bbbe09dd1648bc215f6f2d03f41
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-09-10 13:56:20 +0900

    fix load order problem

--- secret_token.rb
+++ secret_token.rb
@@ -2,6 +2,23 @@
 # usage:
 #  rake rails:template LOCATION=path/to/secret_token.rb
 
+create_file 'lib/secure_token.rb' do
+  <<-RUBY
+require 'securerandom'
+
+def secure_token(token_file_name='.secret')
+  token_file = Rails.root.join(token_file_name)
+  # Use the existing token.
+  File.read(token_file).chomp
+rescue
+  # Generate a new token and store it in token_file.
+  token = SecureRandom.hex(64)
+  File.write(token_file, token, perm: 0600)
+  token
+end
+  RUBY
+end
+
 create_file 'config/initializers/secret_token.rb' do
   <<-RUBY
 # Be sure to restart your server when you modify this file.
@@ -15,27 +32,13 @@ create_file 'config/initializers/secret_token.rb' do
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-require 'securerandom'
-
-def secure_token(token_file_name='.secret')
-  token_file = Rails.root.join(token_file_name)
-  if File.exist?(token_file)
-    # Use the existing token.
-    File.read(token_file).chomp
-  else
-    # Generate a new token and store it in token_file.
-    token = SecureRandom.hex(64)
-    File.write(token_file, token, perm: 0600)
-    token
-  end
-end
-
-Rails.application.config.secret_key_base = ENV['SECRET_TOKEN'] || secure_token
+Rails.application.config.secret_token = ENV['SECRET_TOKEN'] || secure_token
+Rails.application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || secure_token('.secret_key_base')
   RUBY
 end
 
 if File.exist?('config/initializers/devise.rb')
   gsub_file 'config/initializers/devise.rb', /config\.secret_key = .*/ do
-    "config.secret_key = ENV['DEVISE_SECRET_KEY'] || secure_token('.devise_secret_key')"
+    "config.secret_key = ENV['DEVISE_SECRET_KEY'] || (require 'secure_token'; secure_token('.devise_secret_key'))"
   end
 end
