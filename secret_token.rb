cec933d03bce5a7ba8a250ab4fa34ac9e0e18fcf
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-10-02 11:20:38 +0900

    use own directory instead of Rails.root

--- secret_token.rb
+++ secret_token.rb
@@ -8,13 +8,15 @@ create_file 'lib/secure_token.rb' do
   <<-RUBY
 require 'securerandom'
 
-def secure_token(token_file_name='.secret')
-  token_file = Rails.root.join(token_file_name)
+def secure_token(token_file_name, secret_dir='.secret_token')
+  token_file = Rails.root.join(secret_dir, token_file_name)
   # Use the existing token.
   File.read(token_file).chomp
 rescue
   # Generate a new token and store it in token_file.
   token = SecureRandom.hex(64)
+  parent = token_file.parent
+  parent.mkdir(0700) unless parent.exist?
   File.write(token_file, token, perm: 0600)
   token
 end
@@ -34,27 +36,27 @@ create_file 'config/initializers/secret_token.rb' do
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Rails.application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || (require 'secure_token'; secure_token('.secret_key_base'))
+Rails.application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || (require 'secure_token'; secure_token('secret_key_base'))
 # for rails 3
-#Rails.application.config.secret_token = ENV['SECRET_TOKEN'] || secure_token
+#Rails.application.config.secret_token = ENV['SECRET_TOKEN'] || secure_token('secret_token')
   RUBY
 end
 
-unless /secret token/ =~ File.read('.gitignore')
+unless /secret_token/ =~ File.read('.gitignore')
   append_file '.gitignore' do
     <<-GITIGNORE
 
 # secret token
-/.*secret*
+/.secret_token/
     GITIGNORE
   end
 end
 
 if File.exist?('config/initializers/devise.rb')
   gsub_file 'config/initializers/devise.rb', /config\.secret_key = .*/ do
-    "config.secret_key = ENV['DEVISE_SECRET_KEY'] || (require 'secure_token'; secure_token('.devise_secret_key'))"
+    "config.secret_key = ENV['DEVISE_SECRET_KEY'] || (require 'secure_token'; secure_token('devise_secret_key'))"
   end
   gsub_file 'config/initializers/devise.rb', /config\.pepper = .*/ do
-    "config.pepper = ENV['DEVISE_PEPPER'] || (require 'secure_token'; secure_token('.devise_pepper'))"
+    "config.pepper = ENV['DEVISE_PEPPER'] || (require 'secure_token'; secure_token('devise_pepper'))"
   end
 end
