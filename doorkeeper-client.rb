16e6804ec3d888f29e665d1f1f6e1e249f13767d
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-10-12 19:07:24 +0900

    reduce indent

--- doorkeeper-client.rb
+++ doorkeeper-client.rb
@@ -97,11 +97,9 @@ module AuthDoorkeeper
   private
 
   def auto_authenticate_omniauth_user!
-    unless current_user
-      session[:user_return_to] = request.original_url
-      redirect_to main_app.user_omniauth_authorize_path(:doorkeeper)
-      return
-    end
+    return if current_user
+    session[:user_return_to] = request.original_url
+    redirect_to main_app.user_omniauth_authorize_path(:doorkeeper)
   end
 end
 RUBY
