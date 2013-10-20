b90657d0d810893d38a1bb3a2de59c8cec9d46e4
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-10-20 17:49:36 +0900

    add example

---
+++ example.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eux
+RAILS_ROOT=$(pwd)
+cd "$(dirname $0)"
+TEMPLATE_DIR=$(pwd)
+cd "$RAILS_ROOT"
+bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/generators.rb" || :
+bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/haml-rails.rb" || :
+bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/heroku.rb" || :
+bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/rspec.rb" || :
+bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/secret_token.rb" || :
