b0f6cc266025dca8b588c62c277801ab196600bb
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-03-19 13:52:37 +0900

    use slim instead of haml

--- example.sh
+++ example.sh
@@ -5,7 +5,7 @@ cd "$(dirname $0)"
 TEMPLATE_DIR=$(pwd)
 cd "$RAILS_ROOT"
 bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/generators.rb" || :
-bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/haml-rails.rb" || :
+bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/slim-rails.rb" || :
 bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/heroku.rb" || :
 bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/rspec.rb" || :
 bundle exec rake rails:template "LOCATION=$TEMPLATE_DIR/secret_token.rb" || :
