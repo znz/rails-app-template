a9ead78e1bbc4b3ee8152f132230c69425e66c19
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-11-10 20:02:20 +0900

    Add Procfile and CHECKS

--- rails5/dokku.rb
+++ rails5/dokku.rb
@@ -12,4 +12,17 @@ create_file 'app.json', <<-'JSON'
 }
 JSON
 
+# copy from default Procfile
+create_file 'Procfile', <<-'PROCFILE'
+console: bin/rails console
+rake: bundle exec rake
+web: bin/rails server -p $PORT -e $RAILS_ENV
+worker: bundle exec rake jobs:work
+PROCFILE
+
+create_file 'CHECKS', <<-'CHECKS'
+ATTEMPTS=20
+/robots.txt  the robots.txt file
+CHECKS
+
 git_commit 'Add dokku related files'
