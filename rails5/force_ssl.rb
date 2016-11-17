2a017a421d5613c15b8163eb605dad008105ff44
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-11-17 15:49:29 +0900

    See NO_FORCE_SSL environment variable

--- rails5/force_ssl.rb
+++ rails5/force_ssl.rb
@@ -2,5 +2,9 @@
 # frozen_string_literal: true
 require_relative 'util'
 
-uncomment_lines 'config/environments/production.rb', 'config.force_ssl = true'
-git_commit 'Enable force_ssl'
+gsub_file 'config/environments/production.rb', <<-'RUBY', <<-'RUBY'
+  # config.force_ssl = true
+RUBY
+  config.force_ssl = ENV['NO_FORCE_SSL'].blank?
+RUBY
+git_commit 'Enable force_ssl unless NO_FORCE_SSL set'
